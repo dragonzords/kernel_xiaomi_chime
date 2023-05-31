e8594f532cfc60069203dd7853382d07db307127
Peter Zijlstra <peterz@infradead.org>
2023-05-31 13:58:45 +0200

    sched/fair: Commit to lag based placement
    Removes the FAIR_SLEEPERS code in favour of the new LAG based
    placement.
    
    Specifically, the whole FAIR_SLEEPER thing was a very crude
    approximation to make up for the lack of lag based placement,
    specifically the 'service owed' part. This is important for things
    like 'starve' and 'hackbench'.
    
    One side effect of FAIR_SLEEPER is that it caused 'small' unfairness,
    specifically, by always ignoring up-to 'thresh' sleeptime it would
    have a 50%/50% time distribution for a 50% sleeper vs a 100% runner,
    while strictly speaking this should (of course) result in a 33%/67%
    split (as CFS will also do if the sleep period exceeds 'thresh').
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20230531124604.000198861@infradead.org
    Signed-off-by: Helium-Studio <67852324+Helium-Studio@users.noreply.github.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -1,11 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Only give sleepers 50% of their service deficit. This allows
- * them to run sooner, but does not allow tons of sleepers to
- * rip the spread apart.
- */
-#define SCHED_FEAT_FAIR_SLEEPERS 1
-SCHED_FEAT(GENTLE_FAIR_SLEEPERS, false)
 
 /*
  * Using the avg_vruntime, do the right thing and preserve lag across
